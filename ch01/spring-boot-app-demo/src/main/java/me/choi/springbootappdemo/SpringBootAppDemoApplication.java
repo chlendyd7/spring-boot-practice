7799fc30f8d87bb79f7872b678097f25d6d61b23
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-26 14:31:28 +0900

    feat: 스프링 웹 애플리케이션 타입 지정
    개발자가 애플레키에션 타입을 알고 있다면 직접 타입을 지정 할 수 있음

--- ch01/spring-boot-app-demo/src/main/java/me/choi/springbootappdemo/SpringBootAppDemoApplication.java
+++ ch01/spring-boot-app-demo/src/main/java/me/choi/springbootappdemo/SpringBootAppDemoApplication.java
@@ -1,13 +1,18 @@
 package me.choi.springbootappdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAppDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppDemoApplication.class, args);
+		SpringApplication springApplication =
+			new SpringApplication(SpringBootAppDemoApplication.class);
+
+		springApplication.setWebApplicationType(WebApplicationType.REACTIVE);
+		springApplication.run(args);
 	}
 
 }
