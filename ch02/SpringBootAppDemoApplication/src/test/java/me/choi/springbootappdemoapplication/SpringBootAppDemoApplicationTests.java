ab51fdd2bdc85c85386c14c86006ddf05e0fe041
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-26 21:05:21 +0900

    feat: setDefaultProperties
    spring.config.import에 명시한 파일이 존재하지 않아도 스프링 부트는 파일을 무시하고 나머지 앱 기동 작업 계속 수행 한다.

---
+++ ch02/SpringBootAppDemoApplication/src/test/java/me/choi/springbootappdemoapplication/SpringBootAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.springbootappdemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
