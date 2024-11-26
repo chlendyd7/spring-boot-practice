06920bd606d6b9a81d9f28b006381e663b76f26b
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-26 21:59:33 +0900

    feat: 시스템 환경 변수를 이용
    우선순위
    1. SpringApplication
    2. @PropertySource
    3. 설정 정보 파일
    4. 운영 체제 환경 변수
    5. 명령행 인자

---
+++ ch02/os-env-variables/src/main/java/me/choi/osenvvariables/OsEnvVariablesApplication.java
@@ -0,0 +1,22 @@
+package me.choi.osenvvariables;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class OsEnvVariablesApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(OsEnvVariablesApplication.class);
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext =
+				SpringApplication.run(OsEnvVariablesApplication.class, args);
+		Environment env = applicationContext.getBean(Environment.class);
+		log.info("Configured application timeout value: "+ env.getProperty("app.timeout"));
+	}
+
+}
---
+++ ch02/os-env-variables/src/test/java/me/choi/osenvvariables/OsEnvVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.osenvvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsEnvVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
