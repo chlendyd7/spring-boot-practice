4e9251f90a3bc31aba227491427a4a74bf5134f2
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-26 14:24:22 +0900

    ch01 init

---
+++ ch01/spring-boot-app-demo/src/main/java/me/choi/springbootappdemo/SpringBootAppDemoApplication.java
@@ -0,0 +1,13 @@
+package me.choi.springbootappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppDemoApplication.class, args);
+	}
+
+}
---
+++ ch01/spring-boot-app-demo/src/test/java/me/choi/springbootappdemo/SpringBootAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.springbootappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
