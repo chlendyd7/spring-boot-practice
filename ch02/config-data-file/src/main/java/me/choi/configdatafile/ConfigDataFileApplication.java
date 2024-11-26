e9f9a611900e2543f14ef21fce905ff8ef467cf0
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-26 21:34:23 +0900

    feat: Optional config 설정 방법
    java -jar target\config-data-file-0.0.1-SNAPSHOT.jar --spring.config.location=data/sbip.yml
    
    optional을 붙히면 없어도 정상 실행

---
+++ ch02/config-data-file/src/main/java/me/choi/configdatafile/ConfigDataFileApplication.java
@@ -0,0 +1,13 @@
+package me.choi.configdatafile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDataFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigDataFileApplication.class, args);
+	}
+
+}
